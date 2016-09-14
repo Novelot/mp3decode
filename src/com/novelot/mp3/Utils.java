eaea97ac6c3e3f1034f10d8253432e9ba3df82ec
liuyl <liuyl@kaolafm.com>
2016-09-14 17:05:52 +0800

    1.加入Slot计算方法；

--- src/com/novelot/mp3/Utils.java
+++ src/com/novelot/mp3/Utils.java
@@ -18,4 +18,30 @@ public class Utils {
 
 	}
 
+	/**
+	 * 获取Slot个数
+	 * 
+	 * @return
+	 */
+	public static int getSlotSize(int layer, int bitrate, int sampling_frequency) {
+		// Layer I:
+		// N=12*bitrate/sampling_frequency;
+		// Layer II:
+		// N=144*bitrate/sampling_frequency;
+		int N = 0;
+		if (layer == 1) {
+			N = 12 * bitrate / sampling_frequency;
+		} else if (layer == 2) {
+			N = 144 * bitrate / sampling_frequency;
+		}
+		return N;
+	}
+
+	public static int getBitrate() {
+
+	}
+
+	public static int getSamplingFrequency() {
+
+	}
 }
